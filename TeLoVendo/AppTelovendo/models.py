de106cfe7d8c061029ea722bbf08d3ccefa1e908
IanHernandez95 <ian.hernandez.f@gmail.com>
2022-04-18 21:18:28 -0400

    Se agrega template base y clase cliente en models

--- TeLoVendo/AppTelovendo/models.py
+++ TeLoVendo/AppTelovendo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=60)
+    edad = models.IntegerField()
+    mail = models.EmailField(verbose_name="Ingrese Correo del Cliente")
+    telefono = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
